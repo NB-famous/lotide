fd73b186841b99dca67259270f58e4823a9b1970
Nikko B <ncbadoles@gmail.com>
2020-07-27 12:37:35 +0000

    there are the test files for modules

---
+++ test/headTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+  // TEST CODE
+  console.log(head([5,6,7]));
+  console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+
