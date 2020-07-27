d2e637bb4c5358640cbef7f2d3d93c9c31b76ab7
Nikko B <ncbadoles@gmail.com>
2020-07-27 18:33:12 +0000

    added package.json to lotide and mod headTest.js to containt mocha and chai checks

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,25 @@
-const assertEqual = require('../assertEqual');
+////////////// Using this as References //////////// Can be deleted -> no longer needed due to chai
+
+/* const assertEqual = require('../assertEqual');
 const head = require('../head');
 
   // TEST CODE
   console.log(head([5,6,7]));
-  console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+  console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")); */
+
+
+///////////IMPLEMENTING MOCHA AND CHAI TEST SOLUTIONS ///////////////
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
 
