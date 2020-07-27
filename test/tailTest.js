8518c843e844ecf54a97fb1a8ff6eba2a0c33275
Nikko B <ncbadoles@gmail.com>
2020-07-27 14:03:38 +0000

    added tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+
+//const newTestArr = ["Lighthouse", "Labs", "Nick"];//?
+
+console.log(tail(["Hello", "Lighthouse", "Labs", "Nick"]));
+//console.log(assertEqual(tail(["Hello","Lighthouse", "Labs", "Nick"]), ["Lighthouse", "Labs", "Nick"])); // 
